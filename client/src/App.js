ef82c1292e59e9da7600eddf503799c5e4f2701f
ashiqa <rahmanashiqa@gmail.com>
2021-11-22 20:32:06 +0300

    nodejs

--- client/src/App.js
+++ client/src/App.js
@@ -1,168 +1,35 @@
-import './style.css' ;
-import Header from './header';
-import BoardHeader from './boardheader';
-import PostForm from './postform';
+import './style.css';
+import AuthModalContext from "./AuthModalContext";
+import {useState,useEffect} from "react";
+import axios from 'axios';
+import UserContext from "./UserContext";
+import Routing from "./Routing";
+import PostFormModalContext from "./PostFormModalContext";
 
 function App() {
+  const [showAuthModal,setShowAuthModal] = useState(false);
+  const [showPostFormModal,setShowPostFormModal] = useState(false);
+  const [user,setUser] = useState({});
+
+  useEffect(() => {
+
+    axios.get('http://localhost:4000/user', {withCredentials:true})
+      .then(response => setUser(response.data));
+
+  }, []);
+  function logout() {
+    axios.post('http://localhost:4000/logout', {}, {withCredentials:true})
+      .then(() => setUser({}));
+  }
+
   return (
-    <div>
-      <Header/>
-      <AuthModal/>
-      <BoardHeader/>
-      <PostForm/>
-      <div className="m-4 px-6 bg-rddit_dark text-reddit_text">
-      <div className="border border-reddit border bg-reddit_dark-brighter p-5 rounded-md">
-        <h5 className="text-reddit_text-darker text-sm mb-1">Posted by u/test12 5 hours ago</h5>
-        <h2 className="text-xl mb-4">Vegetable Broth Recipe....</h2>
-        <div className="text-sm leading-6">ook, I was gonna go easy on you
-Not to hurt your feelings
-But I'm only going to get this one chance
-Something's wrong, I can feel it
-(Six minutes)
-Just a feeling I've got
-Like something's about to happen
-But I don't know what
-If that means what I think it means
-We're in trouble, big trouble
-And if he is as bananas as you say
-I'm not taking any chances
-You are just what the doc ordered
-I'm beginnin' to feel like a Rap God, Rap God
-All my people from the front to the back nod, back nod
-Now, who thinks their arms are long enough
-To slap box, slap box?
-They said I rap like a robot, so call me Rap-bot
-But for me to rap like a computer
-It must be in my genes
-I got a laptop in my back pocket
-My pen'll go off when I half-cock it
-Got a fat knot from that rap profit
-Made a livin' and a killin' off it
-Ever since Bill Clinton was still in office
-With Monica Lewinsky feelin' on his nutsack
-I'm an MC still as honest
-But as rude and as indecent as all hell
-Syllables, skill-a-holic (Kill 'em all with)
-This flippity dippity-hippity hip-hop
-You don't really wanna get into a pissin' match
-With this rappity brat, packin' a MAC
-In the back of the Ac'
-Backpack rap crap, yap-yap, yackety-yack
-And at the exact same time
-I attempt these lyrical acrobat stunts
-While I'm practicin' that
-I'll still be able to break a motherfuckin' table
-Over the back of a couple of faggots and crack it in half
-Only realized it was ironic
-I was signed to Aftermath after the fact
-How could I not blow?
-All I do is drop F-bombs
-Feel my wrath of attack
-Rappers are havin' a rough time period
-Here's a maxi pad
-It's actually disastrously bad for the wack
-While I'm masterfully constructing this masterpiece as
-'Cause I'm beginnin' to feel like a Rap God, Rap God
-All my people from the front to the back nod, back nod
-Now, who thinks their arms are long enough to slap box, slap box?
-Let me show you maintainin' this shit ain't that hard, that hard
-Everybody wants the key and the secret
-To rap immortality like Ι have got
-Well, to be truthful the blueprint's
-Simply rage and youthful exuberance
-Everybody loves to root for a nuisance
-Hit the Earth like an asteroid
-Did nothing but shoot for the Moon since (Pew!)
-MCs get taken to school with this music
-'Cause I use it as a vehicle to "bus the rhyme"
-Now I lead a new school full of students
-Me? I'm a product of Rakim
-Lakim Shabazz, 2Pac, N.W.A, Cube, hey Doc, Ren
-Yella, Eazy, thank you, they got Slim
-Inspired enough to one day grow up
-Blow up and be in a position
-To meet Run–D.M.C., induct them
-Into the motherfuckin' Rock and Roll Hall of Fame
-Even though I'll walk in the church
-And burst in a ball of flames
-Only Hall of Fame I'll be inducted in
-Is the alcohol of fame
-On the wall of shame
-You fags think it's all a game
-'Til I walk a flock of flames
-Off a plank and, tell me what in the fuck are you thinkin'?
-Little gay-lookin' boy
-So gay I can barely say it with a straight face
-Lookin' boy (Ha-ha!)
-You're witnessin' a massacre
-Like you're watching a church gathering take place, lookin' boy
-"Oy vey, that boy's gay!"—that's all they say, lookin' boy
-You get a thumbs up, pat on the back
-And a "way to go" from your label every day, lookin' boy
-Hey, lookin' boy! What you say, lookin' boy?
-I get a "hell yeah" from Dre, lookin' boy
-I'ma work for everything I have
-Never asked nobody for shit
-Get outta my face, lookin' boy!
-Basically, boy, you're never gonna be capable
-Of keepin' up with the same pace, lookin' boy, 'cause—
-I'm beginnin' to feel like a Rap God, Rap God
-All my people from the front to the back nod, back nod
-The way I'm racin' around the track
-Call me NASCAR, NASCAR
-Dale Earnhardt of the trailer park, the White Trash God
-Kneel before General Zod
-This planet's Krypton—no, Asgard, Asgard
-So you'll be Thor, I'll be Odin
-You rodent, I'm omnipotent
-Let off, then I'm reloadin'
-Immediately with these bombs, I'm totin'
-And I should not be woken
-I'm the walkin' dead, but I'm just a talkin' head
-A zombie floatin'
-But I got your mom deep-throatin'
-I'm out my Ramen Noodle
-We have nothin' in common, poodle
-I'm a Doberman, pinch yourself in the arm
-And pay homage, pupil
-It's me, my honesty's brutal
-But it's honestly futile if I don't
-Utilize what I do though
-For good at least once in a while
-So I wanna make sure somewhere in this
-Chicken scratch I scribble and doodle enough rhymes
-To maybe try to help get some people through tough times
-But I gotta keep a few punchlines
-Just in case, 'cause even you unsigned
-Rappers are hungry lookin' at me like it's lunchtime
-I know there was a time where once I
-Was king of the underground
-But I still rap like I'm on my Pharoah Monch grind
-So I crunch rhymes, but sometimes when you combine
-Appeal with the skin color of mine
-You get too big and here they come tryin'
-To censor you like that one line
-I said on "I'm Back" from The Mathers LP 1 when I
-Tried to say I'll take seven kids from Columbine
-Put 'em all in a line, add an AK-47, a revolver and a 9
-See if I get away with it now
-That I ain't as big as I was, but I'm
-Morphin' into an immortal, comin' through the portal
-You're stuck in a time warp from 2004 though
-And I don't know what the fuck that you rhyme for
-You're pointless as Rapunzel with fuckin' cornrows
-You write normal? Fuck being normal!
-And I just bought a new raygun from the future
-Just to come and shoot ya, like when Fabolous made Ray J mad
-'Cause Fab said he looked like a fag at Mayweather's pad
-Singin' to a man while he played piano
-Man, oh man, that was a 24-7 special on the cable channel
-So Ray J went straight to the radio station
-The very next day, "Hey Fab, I'ma kill you!"</div>
-      </div>
-      </div>
-      
-    </div>
+    <AuthModalContext.Provider value={{show:showAuthModal,setShow:setShowAuthModal}}>
+      <PostFormModalContext.Provider value={{show:showPostFormModal,setShow:setShowPostFormModal}}>
+        <UserContext.Provider value={{...user, logout, setUser}}>
+          <Routing />
+        </UserContext.Provider>
+      </PostFormModalContext.Provider>
+    </AuthModalContext.Provider>
   );
 }
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,3 @@
-
-  
 module.exports = {
   purge: ['./src/*{.js}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
